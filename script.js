622baccedf4db3a6cbc3869df82118273febf602
Kagiso-Obed-Makubjana <deepercreatives@gmail.com>
2026-03-08 11:11:13 +0200

    Adjust scroll distance for next button in carousel

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 
 nextBtn.addEventListener("click", () => {
-  carousel.scrollBy({ left: 300, behavior: 'smooth' });
+  carousel.scrollBy({ left: 310, behavior: 'smooth' });
 });
 prevBtn.addEventListener("click", () => {
   carousel.scrollBy({ left: -300, behavior: 'smooth' });
